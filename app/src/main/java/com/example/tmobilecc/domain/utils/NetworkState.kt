a660dc2492d9bf85a6e3999667daadef096784b0
Jimmy McBride <mcbride967@gmail.com>
2024-10-09 10:53:54 -0500

    cleaned up network state

--- app/src/main/java/com/example/tmobilecc/domain/utils/NetworkState.kt
+++ app/src/main/java/com/example/tmobilecc/domain/utils/NetworkState.kt
@@ -6,22 +6,9 @@ sealed class NetworkState<out T> {
 	object Idle : NetworkState<Nothing>()
 	object Loading : NetworkState<Nothing>()
 	data class Success<T>(val data: T) : NetworkState<T>()
-	data class Error(val message: String) : NetworkState<Nothing>()
-}
 
-fun <T> NetworkState<T>.duringState(
-	success: (T) -> Unit = {},
-	error: (String) -> Unit = {},
-	idle: () -> Unit = {},
-	loading: () -> Unit = {},
-): NetworkState<T> {
-	when (this) {
-		is NetworkState.Success -> success(this.data)
-		is NetworkState.Error -> error(this.message)
-		is NetworkState.Idle -> idle()
-		is NetworkState.Loading -> loading()
-	}
-	return this
+	// I could make this error handle network errors better by adding status code from the response
+	data class Error(val message: String) : NetworkState<Nothing>()
 }
 
 @Composable
